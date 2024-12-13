2b7528fe400a5a29ed2358bef48e5f1b2663bccc
Rifky Syahreza <rsyahrezafs@gmail.com>
2024-12-13 08:08:32 +0700

    [UPDATE] get review summary now include customer id

--- backend/src/main/java/com/vibetribe/backend/infrastructure/usecase/review/dto/ReviewSummaryDTO.java
+++ backend/src/main/java/com/vibetribe/backend/infrastructure/usecase/review/dto/ReviewSummaryDTO.java
@@ -7,13 +7,15 @@ import lombok.Setter;
 @Setter
 public class ReviewSummaryDTO {
     private String eventName;
+    private Long customerId;
     private String photoProfileUrl;
     private String customerName;
     private Integer rating;
     private String review;
 
-    public ReviewSummaryDTO(String title, String photoProfileUrl, String name, Integer rating, String review) {
+    public ReviewSummaryDTO(String title, Long customerId, String photoProfileUrl, String name, Integer rating, String review) {
         this.eventName = title;
+        this.customerId = customerId;
         this.photoProfileUrl = photoProfileUrl;
         this.customerName = name;
         this.rating = rating;
